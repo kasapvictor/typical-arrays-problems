315dca0ea4fd7b0957afc31aa1868c8d4a46a240
Kasap Victor <kasap.victor@gmail.com>
2021-07-18 16:15:22 +0300

    upd: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr = []) {
+    const data = arr;
+    if ( arr.length === 0 ) {
+        return 0;
+    }
+
+    let res = arr.sort ( function ( a, b ) {
+        return a - b;
+    } );
+
+    console.log(res);
+
+    return res[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max ( arr = []) {
+    if ( arr.length === 0 ) {
+        return 0;
+    }
+
+    let res = arr.sort ( function ( a, b ) {
+        return b - a;
+    } );
+
+    return res[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg ( arr = []) {
+    if ( arr.length === 0 ) {
+        return 0;
+    }
+
+    let res = 0;
+
+    for ( let i = 0; i < arr.length; i++ ) {
+        res += arr[i];
+    }
+
+    return res / arr.length;
 }
